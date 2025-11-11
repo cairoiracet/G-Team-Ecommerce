22d4b3c4500e935d21ca36d4b1791b6483459b6a
Cairo Iracet <cairoiracet4@gmail.com>
2025-11-11 18:22:17 -0300

    começando a componentizar header, footer e containers

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,23 @@
+import Container from "@/components/Container";
 import { Button } from "@/components/ui/button";
 import React from "react";
 
 const Home = () => {
   return (
-    <div className="shop_light_pink">
+    <Container className="shop_light_pink">
       <h2>Home</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce at lectus ut lacus iaculis ullamcorper. Nullam egestas orci ac massa hendrerit, non dapibus massa aliquam. Integer consectetur orci ut ex porttitor hendrerit. Aliquam at quam ipsum. In rutrum turpis et sapien scelerisque sodales. Integer nec urna id leo imperdiet varius. Morbi vehicula porta augue non ullamcorper. Nulla ex neque, pulvinar eu tempus eget, tempor at nibh. Quisque lacus tortor, volutpat ac nibh non, condimentum tristique dui. Nam luctus finibus turpis eu consequat.</p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce at lectus
+        ut lacus iaculis ullamcorper. Nullam egestas orci ac massa hendrerit,
+        non dapibus massa aliquam. Integer consectetur orci ut ex porttitor
+        hendrerit. Aliquam at quam ipsum. In rutrum turpis et sapien scelerisque
+        sodales. Integer nec urna id leo imperdiet varius. Morbi vehicula porta
+        augue non ullamcorper. Nulla ex neque, pulvinar eu tempus eget, tempor
+        at nibh. Quisque lacus tortor, volutpat ac nibh non, condimentum
+        tristique dui. Nam luctus finibus turpis eu consequat.
+      </p>
       <Button>Check Out</Button>
-    </div>
+    </Container>
   );
 };
 
---
+++ components/Header.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Container from "./Container";
+
+const Header = () => {
+  return <header>
+    <Container className="max-w-full">Header</Container>    
+  </header>;
+};
+
+export default Header;
